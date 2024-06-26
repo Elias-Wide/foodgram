1ebe84a4f4ac4dd68130b241a8cfd556c1041e15
Ilya Shirokov <ilshi2904@yandex.ru>
2024-06-26 04:13:32 +0300

    Almost done/need to deploy

--- backend/foodgramm_backend/urls.py
+++ backend/foodgramm_backend/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/s/', include("urlshortner.urls"))
+    path('s/', include("urlshortner.urls"))
 ]
